d3923399b49ca929bafd4d26207e77d3d1528575
Vadim Borovikov <vadim.borovikov@gmail.com>
2020-02-02 10:33:17 +0300

    Add project files.

---
+++ CurrencyAnalyzer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CurrencyAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
